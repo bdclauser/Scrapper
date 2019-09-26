7c2f7da075db5562f7974be47943b140a7561c6e
Brian Clauser <s@mJN0q@Py>
2019-09-26 14:53:18 -0400

    now pulls and prints

--- weather_scrapping.py
+++ weather_scrapping.py
@@ -1,9 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('http://dataquestio.github.io/web-scraping-pages/simple.html')
-print(page)
-print(page.content)
-
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=42.6271&lon=-83.9922#.XY0GM1VKhhE')
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+tonight = forecast_items[0]
+print(tonight.prettify())
+
+period = tonight.find(class_="period-name").get_text()
+short_desc = tonight.find(class_="short-desc").get_text()
+temp = tonight.find(class_="temp").get_text()
+print(period)
+print(short_desc)
+print(temp)
